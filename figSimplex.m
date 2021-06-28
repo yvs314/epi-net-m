4651734daa680a0edd19e3157373dd609f24ddee
Yaroslav Salii <yvs314@gmail.com>
2021-06-28 10:59:33 -0400

    multi-node simplex plot

--- figSimplex.m
+++ figSimplex.m
@@ -1,12 +1,12 @@
-%% plot: Draw a trajectory of one node in the (z,s) simplex
+%% plot: Draw a trajectory of one or more nodes in the (z,s) simplex
 % given two series of susceptible `s` and infected `z` fractions
 % OPT: nodeTag is optional, omit to suppress the title 
 function [outFig] = figSimplex(s_in, z_in, nodeTag)   
         
 outFig = newplot;
-fplot(@(x) 1-x, [0,max(z_in)]);
+fplot(@(x) 1-x, [0,max(z_in,[],'all')]);
 hold on;
-scatter(z_in,s_in);
+plot(z_in',s_in',':+');
 hold off;
     
 %title and axis labels
