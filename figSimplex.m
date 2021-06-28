2297bbe813870827d90c95012cce7f29cc6663ec
Yaroslav Salii <yvs314@gmail.com>
2021-06-28 10:44:11 -0400

    mv for consistency

--- figSimplex.m
+++ figSimplex.m
@@ -1,7 +1,7 @@
 %% plot: Draw a trajectory of one node in the (z,s) simplex
 % given two series of susceptible `s` and infected `z` fractions
 % OPT: nodeTag is optional, omit to suppress the title 
-function [outFig] = pltSimplex(s_in, z_in, nodeTag)   
+function [outFig] = figSimplex(s_in, z_in, nodeTag)   
         
 outFig = newplot;
 fplot(@(x) 1-x, [0,max(z_in)]);
